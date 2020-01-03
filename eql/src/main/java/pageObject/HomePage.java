bd3ca0fab29c06e162d7ccdee81684a6d48089ca
Kevin Marionneau <kmarionneau@henix.fr>
2020-01-03 11:16:51 +0100

    HomePage+PageProjet

--- eql/src/main/java/pageObject/HomePage.java
+++ eql/src/main/java/pageObject/HomePage.java
@@ -1,12 +1,29 @@
 package pageObject;
 
+
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 	
-	@FindBy (partialLinkText = "Ressources")
-	WebElement resourcesMenu;
+	/*@FindBy (partialLinkText = "Ressources")
+	WebElement resourcesMenu;*/
+	@FindBy (xpath="//button[contains(text(),'Calendrier')]")
+	WebElement calendrier;
+	@FindBy (xpath="//a[contains(text(),'Projet')]")
+	WebElement projet;
+	
+	public PageProjet allerSurProjet (WebDriver driver) {
+		Actions a = new Actions(driver);
+		a.moveToElement(calendrier).build().perform();
+		a.moveToElement(projet).build().perform();
+		projet.click();
+		return PageFactory.initElements(driver, PageProjet.class);
+	}
 	
 
 }
