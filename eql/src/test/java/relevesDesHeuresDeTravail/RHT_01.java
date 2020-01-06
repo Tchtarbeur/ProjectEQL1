c9cabbed3ec6fc058a6b80d9e37513da7d53d365
Kevin Marionneau <kmarionneau@henix.fr>
2020-01-06 17:06:18 +0100

    modifs Feuilles de temps

--- eql/src/test/java/relevesDesHeuresDeTravail/RHT_01.java
+++ eql/src/test/java/relevesDesHeuresDeTravail/RHT_01.java
@@ -2,13 +2,21 @@ package relevesDesHeuresDeTravail;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import pageObject.HomePage;
 import pageObject.LoginPage;
@@ -34,7 +42,7 @@ public class RHT_01 {
 	}
 
 	@Test
-	public void test() {
+	public void test() throws ParseException, InterruptedException {
 
 		driver.get(url);
 		LoginPage logPage = PageFactory.initElements(driver, LoginPage.class);
@@ -46,9 +54,9 @@ public class RHT_01 {
 		//Vérification homePage
 		assertTrue(homePage.calendrier.isDisplayed());
 
+							//Pas De Test 2
 		//Aller sur page Liste des feuilles de temps
-		homePage.allerSurFeuilleTemps(driver);
-		PageFeuilleTemps pageFeuilleTemps = PageFactory.initElements(driver, PageFeuilleTemps.class);
+		PageFeuilleTemps pageFeuilleTemps = homePage.allerSurFeuilleTemps(driver);
 
 		//Vérification page Liste des feuilles de temps
 		//Vérifier titre
@@ -71,15 +79,16 @@ public class RHT_01 {
 		assertTrue(pageFeuilleTemps.champCalendrierA.isEnabled());
 
 		//Vérifier boutton filtre
-		assertTrue(pageFeuilleTemps.bouttonFiltre.isEnabled());
+		assertTrue(pageFeuilleTemps.boutonFiltre.isEnabled());
 
 		//Vérifier liste déroulante Choisir un Canevas
 		assertTrue (pageFeuilleTemps.ldChoisirCanevas.isEnabled());
 		assertTrue (pageFeuilleTemps.choixDefaultldChoisircanevas.isSelected());
 
 		//Vérifier boutton Nouvelle feuille de temps
-		assertTrue (pageFeuilleTemps.bouttonNouvelleFeuilleDeTemps.isEnabled());
+		assertTrue (pageFeuilleTemps.boutonNouvelleFeuilleDeTemps.isEnabled());
 
+							//Pas De Test 3
 		//Clique sur Nouvelle feuille de temps
 		pageFeuilleTemps.creerFeuilleTemps(driver);
 		PageCreerFeuilleTemps pageCFT = PageFactory.initElements(driver, PageCreerFeuilleTemps.class);
@@ -92,13 +101,16 @@ public class RHT_01 {
 		assertFalse(pageCFT.blocDonnéesGénéralesChampCode.isEnabled());
 		assertTrue(pageCFT.blocDonnéesGénéralesCase.isSelected());
 		assertEquals("Générer le code", pageCFT.blocDonnéesGénéralesGenererLeCode.getText());
-
+		
+		//Récupération du code pour la suite du test
+		String codeNouvelleFT = pageCFT.blocDonnéesGénéralesChampCode.getAttribute("value");
+		
 		//Vérifier bloc Champs Rubriques
 		assertEquals("Champs Rubriques", pageCFT.blocChampsRubriques.getText());
 
 		//Vérifier bloc Lignes de feuilles de temps
 		assertEquals("Lignes de feuille de temps", pageCFT.blocLignesDeFeuillesDeTemps.getText());
-		assertTrue (pageCFT.bouttonAjouterUneLigne.isEnabled());
+		assertTrue (pageCFT.boutonAjouterUneLigne.isEnabled());
 		assertEquals ("Date", pageCFT.titreColonne1.getText());
 		assertEquals ("Ressource", pageCFT.titreColonne2.getText());
 		assertEquals ("Tâche", pageCFT.titreColonne3.getText());
@@ -114,9 +126,109 @@ public class RHT_01 {
 		assertTrue (pageCFT.btSauvegarder.isEnabled());
 		assertTrue (pageCFT.btAnnuler.isEnabled());
 
+								//Pas De Test 4
 		//Clique boutton Ajouter une ligne
-		pageCFT.bouttonAjouterUneLigne.click();
+		pageCFT.boutonAjouterUneLigne.click();
 
-
+		//Vérification date
+		SimpleDateFormat formatDate= new SimpleDateFormat("d MMM yyyy");
+		Date date = new Date(System.currentTimeMillis());
+		String strDate = formatDate.format(date);
+		String dateValue = pageCFT.champDate.getAttribute("value");
+		assertEquals(strDate , dateValue);
+		
+		//Vérification menu déroulant avec aucune valeur par défaut
+		assertTrue(pageCFT.listeDeroulanteRessources.isDisplayed());
+		String valeurAutocomplete = pageCFT.listeDeroulanteRessources.getAttribute("autocomplete");
+		assertEquals ("off",valeurAutocomplete);
+		
+		//Vérification tache
+		assertTrue (pageCFT.champTache.isEnabled());
+		assertTrue (pageCFT.boutonRechercheTache.isEnabled());
+		
+		//Vérification heure
+		assertTrue (pageCFT.champHeure.isEnabled());
+		String valeurValue = pageCFT.champHeure.getAttribute("value");
+		assertEquals ("0", valeurValue);
+		
+		//Vérification types d'heures
+		assertTrue (pageCFT.listeDeroulanteTypeHeures.isEnabled());
+		assertTrue (pageCFT.choixTypeHeuresDefault.isSelected());
+		
+		//Vérification Réalisé
+		assertFalse (pageCFT.caseRealise.isSelected());
+		
+		//Vérification code
+		assertFalse (pageCFT.champCode.isEnabled());
+		String valeurValueCode = pageCFT.champCode.getAttribute("value");
+		assertEquals ("", valeurValueCode);
+		
+		//Vérification Op
+		assertTrue (pageCFT.boutonSupprimer.isEnabled());
+		String valeurTitle = pageCFT.boutonSupprimer.getAttribute("title");
+		assertEquals ("Supprimer", valeurTitle);
+		
+							//Pas De Test 5
+		//Date
+		SimpleDateFormat formatDate2= new SimpleDateFormat("d MMM yyyy"); 					
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.DAY_OF_MONTH, 3); 													
+		String datePlusCinq = formatDate2.format(c.getTime());
+		TechTools.inputText(pageCFT.champDate, datePlusCinq);
+		
+		//Ressource
+		pageCFT.choixRessource.click();
+		pageCFT.choix1Ressource.click();
+		
+		//Tache
+		pageCFT.boutonRechercheTache.click();
+		pageCFT.choix1Tache.click();
+		
+		//Heure
+		TechTools.inputText(pageCFT.champHeure, "8");
+		
+		//Réalisé
+		pageCFT.caseRealise.click();
+		
+		//Enregistrer
+		PageFeuilleTemps pageFT = pageCFT.enregistrerNouvelleFeuilleTemps(driver);
+		
+		//Vérificaton retour sur la page "Liste des feuilles de temps"
+		assertEquals("Liste des feuilles de temps", pageFT.titre.getText());
+		
+		//Vérification message "Feuille de temps sauvegardée"
+		assertEquals("Feuille de temps sauvegardée", pageFT.messageInfo.getText());
+		
+		//Vérification Feuille de temps créée visible dans le tableau
+		WebElement dernierFTCreee = pageFT.xpathFTVariable(driver, codeNouvelleFT);
+		assertTrue(dernierFTCreee.isDisplayed());
+		
+						//Pas de test 6
+		pageFT.dateDebutDecroissante(driver);
+		
+						//Pas de test 7
+		pageFT.titreColonne2.click();
+		pageFT.dateFinCroissante(driver);
+		
+						//Pas de test 8
+		pageFT.titreColonne2.click();
+		pageFT.dateFinDecroissante(driver);
+		
+						//Pas de test 9
+		pageFT.titreColonne4.click();
+		pageFT.travailTotalCroissant(driver);
+		
+						//Pas de test 10
+		pageFT.titreColonne4.click();
+		pageFT.travailTotalDecroissant(driver);
+		
+						//Pas de test 11
+		pageFT.titreColonne3.click();
+		pageFT.modeleOrdreAplha(driver);
+		
+		
+						//Pas de test 12
+		pageFT.titreColonne3.click();
+		pageFT.modeleOrdreAplhaInverse(driver);
 	}
 }
